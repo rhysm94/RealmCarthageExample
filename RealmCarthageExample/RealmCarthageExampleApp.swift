adde93515371bf134744fc452040c226f24003ee
Rhys Morgan <rhysm@me.com>
2021-06-02 08:42:57 +0100

    Added all files needed for Realm bug report

---
+++ RealmCarthageExample/RealmCarthageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmCarthageExampleApp.swift
+//  RealmCarthageExample
+//
+//  Created by Rhys Morgan on 02/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct RealmCarthageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
